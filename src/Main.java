5e9b7dfae46b6e2d8afd830b1bbdeea5dce755f9
K-Rey <kevbreyes@gmail.com>
2019-04-06 18:52:07 -0700

    running code, added comments

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,10 @@ public class Main {
     public static void main(String[] args) {
 
         int PhilosopherForkAmount = 5;
-        int rounds = 10;
+        int rounds = 1;
 
         System.out.println("Number of rounds: " + rounds);
+        System.out.println("Number of philosophers: " + PhilosopherForkAmount);
 
         Forks[] forks = new Forks[PhilosopherForkAmount];
 
@@ -22,34 +23,35 @@ public class Main {
         Philosopher[] philosophers = new Philosopher[PhilosopherForkAmount];
         for (int i = 0; i < philosophers.length - 1; i++) {
             philosophers[i] = new Philosopher("Philosopher #" + i, forks[i], forks[i + 1], rounds);
-            System.out.println(i);
         }
-        philosophers[philosophers.length - 1] = new Philosopher("Philosopher #" + (philosophers.length - 1), forks[0], forks[philosophers.length - 1], rounds);
-        System.out.println(philosophers.length - 1);
+        philosophers[philosophers.length - 1] = new Philosopher("Philosopher #" + (philosophers.length - 1), forks[0], forks[philosophers.length - 1], rounds); //manually create the last philospher that shares the first and last fork
 
         for (int i = 0; i < philosophers.length; i++) {
             System.out.println("Thread " + i + " has started");
             Thread t = new Thread(philosophers[i]);
-            t.start();
+            t.start();  //starts thread which executes the run function
         }
     }
 }
 
-// State : 2 = Eat, 1 = think
 class Philosopher extends Thread {
     private Forks _leftFork;
     private Forks _rightFork;
 
     private String _name;
-    private int _state;
     private int _rounds;
 
     public Philosopher(String name, Forks _left, Forks _right, int rounds) {
-        this._state = 1;
         this._name = name;
         _leftFork = _left;
         _rightFork = _right;
         _rounds = rounds;
+    }
+
+    public void run() { //overrides Threads run function to change how many rounds the philosophers go through
+        for (int i = 0; i <= _rounds; i++) {
+            eat();
+        }
     }
 
     public void eat() {
@@ -73,17 +75,10 @@ class Philosopher extends Thread {
     }
 
     public void think() {
-        this._state = 1;
         System.out.println(_name + " is thinking");
         try {
             Thread.sleep(1000);
         } catch (InterruptedException ex) {
-        }
-    }
-
-    public void run() {
-        for (int i = 0; i <= _rounds; i++) {
-            eat();
         }
     }
 }
